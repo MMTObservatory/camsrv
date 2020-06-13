fe640147a3c1d43b25a0e671ae663fe8057344c8
T. E. Pickering <te.pickering@gmail.com>
2020-06-13 00:19:23 -0700

    fix tests to use the tornado testing framework; test with camera disconnected because otherwise pytest hangs

--- camsrv/tests/test_apps.py
+++ camsrv/tests/test_apps.py
@@ -1,27 +1,41 @@
 """
 Sanity checks to make sure applications can be instantiated
 """
-from ..camsrv import CAMsrv
-from ..f9wfs import F9WFSsrv
-from ..matcam import MATsrv
-from ..ratcam import RATsrv
+import tornado
+from tornado.testing import AsyncHTTPTestCase, gen_test
+import time
+import signal
+
+from ..camsrv import CAMsrv, SIMSRVPORT
+from ..f9wfs import F9WFSsrv, F9WFSPORT
+from ..matcam import MATsrv, MATCAMPORT
+from ..ratcam import RATsrv, RATCAMPORT
 
 
-def test_base():
-    app = CAMsrv()
-    assert(app is not None)
+class TestSimSrv(AsyncHTTPTestCase):
+    def get_app(self):
+        app = CAMsrv(connect=False)
+        return app
+
+    @gen_test
+    def test_homepage(self):
+        response = yield self.http_client.fetch(self.get_url('/'))
+        self.assertEqual(response.code, 200)
 
 
-def test_f9():
-    app = F9WFSsrv()
-    assert(app is not None)
+class TestF9Srv(TestSimSrv):
+    def get_app(self):
+        app = F9WFSsrv(connect=False)
+        return app
 
 
-def test_mat():
-    app = MATsrv()
-    assert(app is not None)
+class TestMATSrv(TestSimSrv):
+    def get_app(self):
+        app = MATsrv(connect=False)
+        return app
 
 
-def test_rat():
-    app = RATsrv()
-    assert(app is not None)
+class TestRATSrv(TestSimSrv):
+    def get_app(self):
+        app = RATsrv(connect=False)
+        return app
