bece85242787e74bd5a9985f834d78f6c2aae8ac
sswindell <sswindell@hacksaw.mmto.arizona.edu>
2022-05-09 19:36:29 -0700

    Converting f9 stuff to use pyindi (same as f5)

--- camsrv/f9wfs.py
+++ camsrv/f9wfs.py
@@ -13,17 +13,26 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.websocket
 from tornado.log import enable_pretty_logging
-
+import io
 from pathlib import Path
 
+from scipy.ndimage import median_filter
 import logging
 import logging.handlers
 
 from astropy.io import fits
 
-from indiclient.indicam import SimCam, F9WFSCam
+#from indiclient.indicam import SimCam, F9WFSCam
 
-from .camsrv import CAMsrv
+from pyindi.webclient import INDIWebApp
+
+dev = os.environ.get("WFSDEV", False)
+if dev:
+    from header import update_header
+    from camsrv import CAMsrv
+else:
+    from .header import update_header
+    from .camsrv import CAMsrv
 
 enable_pretty_logging()
 logger = logging.getLogger("")
@@ -31,7 +40,10 @@ logger.setLevel(logging.INFO)
 log = logging.getLogger('tornado.application')
 log.setLevel(logging.INFO)
 
-F9WFSPORT = 8787
+if dev:
+    F9WFSPORT = 8788
+else:
+    F9WFSPORT = 8787
 
 __all__ = ['F9WFSsrv', 'main']
 
@@ -55,8 +67,26 @@ class F9WFSsrv(CAMsrv):
             log.info("Setting f/9 WFS camera to its default configuration, full-frame with 1x1 binning...")
             cam.default_config()
 
+    class LatestImageNameHandler(tornado.web.RequestHandler):
+        """
+        Return the last image filename and path to a get
+        request.
+        This is used by the webapp to display the latest
+        image filename.
+        """
+
+        def get(self):
+            if hasattr(self.application, "last_filename"):
+                if self.application.last_filename is None:
+                    self.write("None")
+                else:
+                    self.write(str(self.application.last_filename))
+            else:
+                self.write("None")
+
     def connect_camera(self):
         # check the actual camera
+        return
         self.camera = None
         try:
             self.camera = F9WFSCam(host=self.camhost, port=self.camport)
@@ -75,14 +105,24 @@ class F9WFSsrv(CAMsrv):
         if self.latest_image is not None:
             filename = self.datadir / Path("f9wfs_" + time.strftime("%Y%m%d-%H%M%S") + ".fits")
             self.latest_image.writeto(filename)
+            self.last_filename = filename
 
     def __init__(self, camhost='f9indi', camport=7624, connect=True):
         self.extra_handlers = [
             (r"/wfs_config", self.WFSModeHandler),
             (r"/default_config", self.DefaultModeHandler),
+            (r"/latest_image_name", self.LatestImageNameHandler),
         ]
+        
+        iwa = INDIWebApp(
+            handle_blob=self.new_image,
+            indihost="f9indi.mmto.arizona.edu",
+            indiport=7624
+        )
 
-        super(F9WFSsrv, self).__init__(camhost=camhost, camport=camport, connect=connect)
+        self.extra_handlers.extend(iwa.indi_handlers())
+        self.indiargs = {"device_name": ["*"], "DEFAULT_TEMP": -10.0}
+        super(F9WFSsrv, self).__init__(camhost="badhost", camport=camport, connect=connect)
 
         self.home_template = "f9wfs.html"
 
@@ -100,6 +140,36 @@ class F9WFSsrv(CAMsrv):
         bp_file = pkg_resources.resource_filename(__name__, os.path.join("data", "f9_mask.fits"))
         with fits.open(bp_file) as hdulist:
             self.bad_pixel_mask = hdulist[0].data.astype(bool)
+
+    def new_image(self, blob):
+        """
+        Pyindi blob callback
+
+
+        @param blob
+        The blob object from the indidriver in this case it is the
+        image from the sbig wfs camera.
+        """
+        buff = io.BytesIO(blob['data'])
+        hdulist = fits.open(buff)
+        if hdulist is not None:
+            hdulist = update_header(hdulist)
+            if self.bad_pixel_mask is not None:
+                im = hdulist[0].data
+                if im.shape != self.bad_pixel_mask.shape:
+                    log.warning(
+                        "Wrong readout configuration for\
+                      making bad pixel corrections..."
+                    )
+                else:
+                    blurred = median_filter(im, size=5)
+                    im[self.bad_pixel_mask] = blurred[self.bad_pixel_mask]
+
+            self.latest_image = hdulist[0]
+            self.save_latest()
+
+        else:
+            log.error("Exposure Failed")
 
 
 def main(port=F9WFSPORT):
